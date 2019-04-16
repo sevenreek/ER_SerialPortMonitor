2568cae839260b241c03cda89818757b91990ef6
Wojciech Walewski <w.walewski112@gmail.com>
2019-04-16 22:34:54 +0200

    Initial commit with basic code that reads the serial input and prints it to console and vice versa.

---
+++ SerialPortMonitor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+// portname baudrate
+// portname baudrate parity databits stopbits handshake
+
+class Program
+{
+    public const int RETURN_INCORRECT_ARG = 1;
+    public const int RETURN_OK = 0;
+    static int Main(string[] args)
+    {
+        SerialPortMonitor serialPortMonitor;
+        if (args.Length != 2 && args.Length != 6)
+        {
+            Console.WriteLine("Incorrect number of arguments specified. Please use the following formula:\n\tspm.exe portname baudrate\nor\n\tspm.exe portname baudrate parity databits stopbits handshake");
+            Console.WriteLine("Gave {0} arguments", args.Length);
+            return RETURN_INCORRECT_ARG;
+        }
+        int baudrate;
+        if(!int.TryParse(args[1], out baudrate))
+        {
+            Console.WriteLine("Baudrate argument could not be parsed.");
+            return RETURN_INCORRECT_ARG;
+        }
+        serialPortMonitor = new SerialPortMonitor(args[0], baudrate);
+
+
+
+
+
+        return RETURN_OK;
+    }
+}
+
---
+++ SerialPortMonitor/SerialPortMonitor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+// The default is 8 data bits, no parity, one stop bit.
+// portname baudrate timeout
+// portname baudrate parity databits stopbits handshake timeout
+class SerialPortMonitor
+{
+    public const int DEFAULT_CONFIG_BAUDRATE = 9600;
+    public const Parity DEFAULT_CONFIG_PARITY = Parity.None;
+    public const int DEFAULT_CONFIG_DATABITS = 8;
+    public const StopBits DEFAULT_CONFIG_STOPBITS = StopBits.One;
+    public const Handshake DEFAULT_CONFIG_HANDSHAKE = Handshake.None;
+    public const int DEFAULT_CONFIG_TIMEOUT = 500;
+    public const int RETURN_INCORRECT_ARG = 1;
+    public const int RETURN_OK = 0;
+    bool isRunning;
+    SerialPort serialPort;
+    Thread reader;
+    string msg;
+    StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+    public SerialPortMonitor(string portname, int baudrate) : this(portname, baudrate, DEFAULT_CONFIG_PARITY, DEFAULT_CONFIG_DATABITS, DEFAULT_CONFIG_STOPBITS, DEFAULT_CONFIG_TIMEOUT)
+    {
+    }
+    public SerialPortMonitor(string portname, int baudrate, int timeout) : this(portname, baudrate, DEFAULT_CONFIG_PARITY, DEFAULT_CONFIG_DATABITS, DEFAULT_CONFIG_STOPBITS, timeout)
+    {
+    }
+    public SerialPortMonitor(string portname, int baudrate, Parity parity, int databits, StopBits stopbits, int timeout)
+    {
+        serialPort = new SerialPort(portname, baudrate, parity, databits, stopbits);
+        reader = new Thread(ReaderRead);
+        serialPort.ReadTimeout = timeout;
+        serialPort.WriteTimeout = timeout;
+        serialPort.Open();
+        isRunning = true;
+        reader.Start();
+        Console.WriteLine("SerialPortMonitor running.");
+        while(isRunning)
+        {
+            msg = Console.ReadLine();
+            if (stringComparer.Equals(msg, "quit"))
+            {
+                isRunning = false;
+            }
+            reader.Join();
+            serialPort.Close();
+        }
+    }
+    private void ReaderRead()
+    {
+        while (isRunning)
+        {
+            try
+            {
+                string msg = serialPort.ReadLine();
+                Console.WriteLine(msg);
+            }
+            catch (TimeoutException) { }
+        }
+    }
+}
